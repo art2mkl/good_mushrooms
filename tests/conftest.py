634d841b925d5beafe6551a72a96112e1d10fd0c
BAGUIDOU <baguidou@MacBook-Air-de-BAGUIDOU.local>
2022-06-20 13:51:16 +0200

    add pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,12 @@ import pytest
 
 from app import create_app
 from config import TestingConfig
-
+from flask_admin import Admin
 
 @pytest.fixture
 def client():
-    app = create_app(TestingConfig)
+
+    admin = Admin(name='my-app', template_mode='bootstrap3')
+    app = create_app(TestingConfig, admin)
     with app.test_client() as client:
         yield client
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,11 @@
 from tests.conftest import client
 
-
 def test_should_status_code_ok(client):
-	response = client.get('/')
+	response = client.get('/test')
 	assert response.status_code == 200
+
+def test_server_response(client):
+	response = client.get('/test')
+	data = response.data.decode()
+	print(data)
+	assert data == 'Test Serveur'
