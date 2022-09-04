28dacfff58858293b08326ce3228939dc17bc4da
BAGUIDOU <baguidou@MacBook-Air-de-BAGUIDOU.local>
2022-09-04 22:53:13 +0200

    add exploration page modification and pytest

--- app/static/js/prediction.js
+++ app/static/js/prediction.js
@@ -6,34 +6,6 @@ $('.edible').hide()
 $('.diag').hide()
 
 
-//-------------------------------------------------------------------------------------------------------------
-//-----------------------------------------    FUNCTIONS     --------------------------------------------------
-//-------------------------------------------------------------------------------------------------------------
-
-
-
-function get_models() {
-    //--------------------------------------------------------------------------------------------------------------
-    //Select Models in current project
-    //--------------------------------------------------------------------------------------------------------------         
-        fetch(`${loc_origin}/prediction/choosemodel/`).then( response => {
-            response.json().then( data => {
-                
-                let optionHTML = '';
-                
-                for (let model of data.models) {
-                    optionHTML += '<option value="'+ model.id + '">' + `${model.name} accuracy : ${model.accuracy.toFixed(2)}, precision : ${model.precision.toFixed(2)}` + '</option>';
-                }
-                choose_model.innerHTML = optionHTML;
-
-            });
-        });
-    }
-//get_models()
-
-
-
-
 //Actions sur le formulaire lors de la validation
 $('form').on('submit', e => {
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,29 @@
 from tests.conftest import client
 
 def test_should_status_code_ok(client):
-	response = client.get('/test')
+	response = client.get('/prediction/predict/1/pungent/urban/brown/bruises/')
 	assert response.status_code == 200
 
-def test_server_response(client):
-	response = client.get('/test')
+def test_json_response_ok(client):
+	response = client.get('/prediction/predict/1/pungent/urban/brown/bruises/')
 	data = response.data.decode()
-	print(data)
-	assert data == 'Test Serveur'
+
+	if '{"prediction":' in data:
+		answer='ok'
+	else:
+		answer='ko'
+	print(data)	
+
+	assert answer == 'ok'
+
+def test_json_response_poisoned(client):
+	response = client.get('/prediction/predict/1/pungent/urban/brown/bruises/')
+	data = response.data.decode()
+
+	if 'poisoned' in data:
+		answer='ok'
+	else:
+		answer='ko'
+	print(data)	
+	
+	assert answer == 'ok'
